7fc0ca4d9d647e03d1039c1f92a9ddb85bbfd546
chocolivia <128565993+intanolivia@users.noreply.github.com>
2023-12-13 16:46:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,7 @@
+bisa
+
 import streamlit as st
 from sqlalchemy import text
-
-# Set Streamlit app-wide theme
-st.set_page_config(page_title="Cinema Schedule Management System", page_icon=":movie_camera:", layout="wide")
-
-# Custom color theme
-st.markdown(
-    """
-    <style>
-        .css-1v3fvcr {
-            color: #fff;
-        }
-
-        .css-1uyte9r {
-            background-color: #2a2a72;
-            background-image: linear-gradient(315deg, #2a2a72 0%, #009ffd 74%);
-            color: #fff;
-        }
-
-        .css-1l4w6pd {
-            color: #000;
-        }
-
-        .css-r26lvk {
-            background-color: #f8f9fa;
-            color: #000;
-        }
-
-        .st-bw {
-            color: #2a2a72;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 
 list_genre = ['', 'Sci-Fi', 'Drama', 'Action', 'Crime']
 list_theater_number = ['', '1', '2', '3']
@@ -44,16 +12,12 @@ with conn.session as session:
     query = text('CREATE TABLE IF NOT EXISTS movie_schedule (id SERIAL, movie_title TEXT, genre TEXT, director TEXT, release_date DATE, start_time TIME, end_time TIME, theater_number INT, ticket_price DECIMAL);')
     session.execute(query)
 
-# Filter by Genre
-selected_genre = st.selectbox("Filter by Genre", list_genre)
-data = conn.query(f"SELECT * FROM movie_schedule WHERE genre = '{selected_genre}' ORDER By id;", ttl="0").set_index('id')
-
 st.header('CINEMA SCHEDULE MANAGEMENT SYSTEM')
-
-# Display the filtered data
-st.dataframe(data)
-
 page_cinema = st.sidebar.selectbox("Choose Menu", ["View Cinema Schedule", "Edit Cinema Schedule"])
+
+if page_cinema == "View Cinema Schedule":
+    data = conn.query('SELECT * FROM movie_schedule ORDER By id;', ttl="0").set_index('id')
+    st.dataframe(data)
 
 if page_cinema == "Edit Cinema Schedule":
     if st.button('Add Data'):
@@ -62,6 +26,7 @@ if page_cinema == "Edit Cinema Schedule":
             session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':'', '6':'', '7':'', '8':''})
             session.commit()
 
+    data = conn.query('SELECT * FROM movie_schedule ORDER By id;', ttl="0")
     for _, result in data.iterrows():        
         id = result['id']
         movie_title_lama = result["movie_title"]
