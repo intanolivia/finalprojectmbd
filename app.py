d01b0f9180bb7fe5b8400f6b066e71d63742bf07
chocolivia <128565993+intanolivia@users.noreply.github.com>
2023-12-13 16:56:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,43 @@
 import streamlit as st
 from sqlalchemy import text
 
+# Set page configuration
 st.set_page_config(page_title="Cinema Schedule Management System", page_icon=":movie_camera:", layout="wide")
 
-list_genre = ['', 'Sci-Fi', 'Drama', 'Action', 'Crime']
-list_theater_number = ['', '1', '2', '3']
-
+# Database connection
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://intanoliviaitaliyana:BHs3h0cygXUa@ep-morning-waterfall-53636265.us-east-2.aws.neon.tech/web")
+
+# Create movie_schedule table if not exists
 with conn.session as session:
     query = text('CREATE TABLE IF NOT EXISTS movie_schedule (id SERIAL, movie_title TEXT, genre TEXT, director TEXT, release_date DATE, start_time TIME, end_time TIME, theater_number INT, ticket_price DECIMAL);')
     session.execute(query)
 
+# Sidebar and header
+st.sidebar.title("Menu")
+page_cinema = st.sidebar.radio("", ["View Cinema Schedule", "Edit Cinema Schedule"])
 st.header('🎬 CINEMA SCHEDULE MANAGEMENT SYSTEM')
 
-# Genre Filter
-selected_genre = st.selectbox("Filter by Genre", list_genre)
-# Handling empty string
-selected_genre = selected_genre if selected_genre != '' else None
-data = conn.query(f"SELECT * FROM movie_schedule WHERE genre = :genre ORDER By id;", {'genre': selected_genre}, ttl="0").set_index('id')
+# View Cinema Schedule
+if page_cinema == "View Cinema Schedule":
+    st.subheader("🎥 View Cinema Schedule")
+    data = conn.query('SELECT * FROM movie_schedule ORDER By id;', ttl="0").set_index('id')
+    st.dataframe(data)
 
-# Display the filtered data
-st.dataframe(data)
-
-page_cinema = st.sidebar.selectbox("Choose Menu", ["🎥 View Cinema Schedule", "✏ Edit Cinema Schedule"])
-
-if page_cinema == "🎥 View Cinema Schedule":
-    # Genre Filter for View Cinema Schedule
-    selected_genre_view = st.selectbox("Filter by Genre", list_genre)
-    # Handling empty string
-    selected_genre_view = selected_genre_view if selected_genre_view != '' else None
-    data_view = conn.query(f"SELECT * FROM movie_schedule WHERE genre = :genre ORDER By id;", {'genre': selected_genre_view}, ttl="0").set_index('id')
-    st.dataframe(data_view)
-
-if page_cinema == "✏ Edit Cinema Schedule":
+# Edit Cinema Schedule
+if page_cinema == "Edit Cinema Schedule":
+    st.subheader("✏ Edit Cinema Schedule")
+    
+    # Add Data Button
     if st.button('Add Data'):
         with conn.session as session:
             query = text('INSERT INTO movie_schedule (movie_title, genre, director, release_date, start_time, end_time, theater_number, ticket_price) VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
             session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':'', '6':'', '7':'', '8':''})
             session.commit()
 
-    for _, result in data.iterrows():        
+    # Display data and provide form for editing
+    data = conn.query('SELECT * FROM movie_schedule ORDER By id;', ttl="0")
+    for _, result in data.iterrows():
         id = result['id']
         movie_title_lama = result["movie_title"]
         genre_lama = result["genre"]
@@ -51,19 +48,20 @@ if page_cinema == "✏ Edit Cinema Schedule":
         theater_number_lama = result["theater_number"]
         ticket_price_lama = result["ticket_price"]
 
-        with st.expander(f'Movie {movie_title_lama}'):
+        with st.expander(f'Movie {movie_title_lama}', expanded=False):
             with st.form(f'movie-data-{id}'):
+                # Form Inputs
                 movie_title_baru = st.text_input("Movie Title", movie_title_lama)
-                genre_baru = st.selectbox("Genre", ["Sci-Fi", "Drama", "Action", "Crime"])
+                genre_baru = st.selectbox("Genre", ["Sci-Fi", "Drama", "Action", "Crime"], index=list_genre.index(genre_lama))
                 director_baru = st.text_input("Director", director_name_lama)
                 release_date_baru = st.date_input("Release Date", release_date_lama)
                 start_time_baru = st.time_input("Start Time", start_time_lama)
                 end_time_baru = st.time_input("End Time", end_time_lama)
-                theater_number_baru = st.selectbox("Theater Number",["1", "2", "3"])
+                theater_number_baru = st.selectbox("Theater Number", ["1", "2", "3"], index=list_theater_number.index(str(theater_number_lama)))
                 ticket_price_baru = st.number_input("Ticket Price", ticket_price_lama)
-                
-                col1, col2 = st.columns([1, 6])
 
+                # Form Submission Buttons
+                col1, col2 = st.columns([1, 6])
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
@@ -75,7 +73,7 @@ if page_cinema == "✏ Edit Cinema Schedule":
                                                     '5':start_time_baru, '6':end_time_baru, '7':theater_number_baru, '8':ticket_price_baru, '9':id})
                             session.commit()
                             st.experimental_rerun()
-                
+
                 with col2:
                     if st.form_submit_button('DELETE'):
                         query = text(f'DELETE FROM movie_schedule WHERE id=:1;')
