7cb8f3e0ce9cadf956bf5610754b8742578c800b
chocolivia <128565993+intanolivia@users.noreply.github.com>
2023-12-15 01:06:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,20 +22,9 @@ if page_cinema == "🎥 View Cinema Schedule":
 if page_cinema == "✏ Edit Cinema Schedule":
     if st.button('Add Data'):
         with conn.session as session:
-            query = text('INSERT INTO movie_schedule (movie_title, genre, director, release_date, start_time, end_time, theater_number, ticket_price) VALUES (:movie_title, :genre, :director, :release_date, :start_time, :end_time, :theater_number, :ticket_price);')
-              data = {
-                'movie_title': '',
-                'genre': '',
-                'director': '',
-                'release_date': None,
-                'start_time': None,
-                'end_time': None,
-                'theater_number': '',
-                'ticket_price': None
-            }
-            session.execute(query, data)
+            query = text('INSERT INTO movie_schedule (movie_title, genre, director, release_date, start_time, end_time, theater_number, ticket_price) VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
+            session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':'', '6':'', '7':'', '8':''})
             session.commit()
-          
 
     data = conn.query('SELECT * FROM movie_schedule ORDER By id;', ttl="0")
     for _, result in data.iterrows():        
